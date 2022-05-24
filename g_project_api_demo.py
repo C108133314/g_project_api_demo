b774fa21487199a2f699e3d8d59c03bf5a7343e3
C108133314 <C108133314@nkust.edu.tw>
2022-05-25 02:43:04 +0800

    add dns

--- g_project_api_demo.py
+++ g_project_api_demo.py
@@ -1,4 +1,4 @@
 import requests
 
-result = requests.post("http://127.0.0.1:8000/get_aspect_api/", data = {"userInput" : "房間環境有待加強，有味道"})
+result = requests.post("https://www.hoteller.online/get_aspect_api/", data = {"userInput" : "房間環境有待加強，有味道"})
 print(result)
